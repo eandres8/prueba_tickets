b79c61389ce1df53d409d39eb3c537d9f167dade
andres vega <elb.andres8@gmail.com>
2020-05-21 23:38:37 -0500

    Configuracion de conexión a mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,19 @@ import { AppService } from './app.service';
 
 // Modules
 import { TicketsModule } from './tickets/tickets.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TicketsModule,
-    MongooseModule.forRoot('mongodb://localhost/tickets', {
-      useNewUrlParser: true
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+      auth: {
+        user: 'root',
+        password: 'example'
+      },
+      dbName: 'tickets',
     }),
   ],
   controllers: [AppController],
