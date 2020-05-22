52f1aee3667ce0e201aba907aa7748a8f847ba25
andres vega <elb.andres8@gmail.com>
2020-05-22 01:27:12 -0500

    Agrego documentación Swagger

--- src/dto/tickets.dto.ts
+++ src/dto/tickets.dto.ts
@@ -1,13 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTicketDTO {
+    @ApiProperty()
     address: string;
+    @ApiProperty({ description: 'Fecha de programación del ticket', type: Date})
     date_programing: Date;
+    @ApiProperty()
     type: 'instalation' | 'maintenance';
+    @ApiProperty()
     description: string;
+    @ApiProperty()
     client: string;
 }
 
 export class UpdateTicketDTO {
+    @ApiProperty()
     state: "pending" | "done" | "canceled" | "rejected";
+    @ApiProperty()
     expert_comment: string;
 }
--- src/dto/users.dto.ts
+++ src/dto/users.dto.ts
@@ -1,16 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDTO {
+    @ApiProperty()
     first_name: string;
+    @ApiProperty()
     last_name: string;
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     address: string;
+    @ApiProperty()
     phone: string;
+    @ApiProperty()
     password: string;
+    @ApiProperty()
     role: "client" | "expert";
+    @ApiProperty()
     state: "active" | "inactive";
 }
 
 export class LoginUserDTO {
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     password: string;
 }
